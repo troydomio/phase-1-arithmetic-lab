46be6c7cae877bca61c77af30fcbbbdd165d5b3e
Troy <troyakins99@yahoo.com>
2021-06-29 18:55:00 -0500

    commit tree 08ad4b024e9ddf58a57c017a3ad3992a2d226fac

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
-// Write your code here
+let num1 = 31, num2 = 2, multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*1000);
+
+let num3= 28, num4= 4, mod = num4 % num3;
+
+let max = Math.max(20);
